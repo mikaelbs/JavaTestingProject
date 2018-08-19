6bdc166d09c2df6be83bc5129723b77267843857
mikaelbs <mikaelbullstee@gmail.com>
2018-08-19 14:46:25 +0200

    second commit

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
 
         persons.add(new Person("Mikael",31,80.2,true,Gender.MALE));
         persons.add(new Person("Christine",27,60.2,true,Gender.FEMALE));
+        persons.add(new Person("Gunnar",50,130,false,Gender.FEMALE));
 
         persons.forEach(person -> System.out.println(person.name));
 
